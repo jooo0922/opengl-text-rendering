65385d76c53ee12964203a72d950a02418313f77
jooo0922 <jooo0922@gmail.com>
2024-12-07 00:27:12 +0900

    feat: implement callbacks

--- src/main.cpp
+++ src/main.cpp
@@ -14,7 +14,33 @@
 #include <string>
 #include <map>
 
+/** 콜백함수 전방 선언 */
+
+// GLFW 윈도우 resizing 콜백함수
+void framebuffer_size_callback(GLFWwindow *window, int width, int height);
+
+// GLFW 윈도우 키 입력 콜백함수
+void processInput(GLFWwindow *window);
+
 int main()
 {
   return 0;
+}
+
+/** 콜백함수 구현부 */
+
+// GLFW 윈도우 키 입력 콜백함수
+void processInput(GLFWwindow *window)
+{
+  // ESC 키 입력 시 렌더링 루프 및 프로그램 종료
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS)
+  {
+    glfwSetWindowShouldClose(window, true);
+  }
+}
+
+// GLFW 윈도우 resizing 콜백함수
+void framebuffer_size_callback(GLFWwindow *window, int width, int height)
+{
+  glViewport(0, 0, width, height);
 }

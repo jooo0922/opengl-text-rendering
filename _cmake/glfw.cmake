064d93c3b0ff61cd8e87a76de85207e9fe93e58b
jooo0922 <jooo0922@gmail.com>
2024-12-06 23:43:47 +0900

    chore: add subs cmake

---
+++ _cmake/glfw.cmake
@@ -0,0 +1,25 @@
+cmake_minimum_required(VERSION 3.18)
+
+include(FetchContent)
+Set(FETCHCONTENT_QUIET FALSE)
+
+FetchContent_Declare(
+  glfw
+  GIT_REPOSITORY https://github.com/glfw/glfw.git
+  GIT_TAG 3.3.9
+)
+
+FetchContent_GetProperties(glfw)
+
+if(NOT glfw_POPULATED)
+  FetchContent_MakeAvailable(glfw)
+  set(GLFW_BUILD_DOCS OFF CACHE BOOL "" FORCE)
+  set(GLFW_BUILD_TESTS OFF CACHE BOOL "" FORCE)
+  set(GLFW_BUILD_EXAMPLES OFF CACHE BOOL "" FORCE)
+endif()
+
+set(glfw_INCLUDE ${glfw_SOURCE_DIR}/include)
+
+message(STATUS "${Green} GLFW Should Be Downloaded ${ColourReset}")
+
+# required modules (for ubuntu 22.04 LTS)
---
+++ _cmake/glm.cmake
@@ -0,0 +1,21 @@
+cmake_minimum_required(VERSION 3.18)
+
+include(FetchContent)
+Set(FETCHCONTENT_QUIET FALSE)
+
+FetchContent_Declare(glm
+  GIT_REPOSITORY https://github.com/g-truc/glm.git
+  GIT_PROGRESS TRUE
+  GIT_TAG 0.9.9.8)
+
+FetchContent_GetProperties(glm)
+
+if(NOT glm_POPULATED)
+  set(GLM_FORCE_CXX11 1)
+  FetchContent_MakeAvailable(glm)
+  set(GLM_TEST_ENABLE OFF CACHE BOOL "" FORCE)
+endif()
+
+set(glm_INCLUDE ${glm_SOURCE_DIR})
+
+message(STATUS "${Green} GLM Should Be Downloaded ${ColourReset}")

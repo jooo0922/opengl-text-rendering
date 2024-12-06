0fe3d5ec52b7d588924561e1558d06e59e0d0275
jooo0922 <jooo0922@gmail.com>
2024-12-07 00:21:23 +0900

    feat: include deps

--- src/main.cpp
+++ src/main.cpp
@@ -1,4 +1,18 @@
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <ft2build.h>
+#include FT_FREETYPE_H
+
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
+#include <shader/shader.hpp>
+
 #include <iostream>
+#include <string>
+#include <map>
 
 int main()
 {
